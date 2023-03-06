c4d92d15d6f118600c72e20181f4754254b486ae
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-03-06 15:17:07 +0530

    modernize-use-using

--- src/merge.hh
+++ src/merge.hh
@@ -1,5 +1,7 @@
 #ifndef MERGE_H
 #define MERGE_H
 #include "decomp.hh"
+
 auto merge(DecompData &decompdata) -> std::set<Face *>;
+
 #endif
--- src/typedefs.hh
+++ src/typedefs.hh
@@ -2,13 +2,13 @@
 #define TYPEDEFS_H
 #include <cstddef>
 #include <cstdint>
-typedef size_t usize;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+using usize = size_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
 #endif
