3b0444a3fbe2b374010b087ac2602e43348aebef
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-03-24 13:57:56 +0530

    add latex images for report

--- utils/vis.py
+++ utils/vis.py
@@ -1,3 +1,12 @@
+import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+from PyQt5 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
 import matplotlib.pyplot as plt
 coord = list()
 n = int(input())
